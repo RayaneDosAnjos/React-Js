431f8911d2c50129083f6e015cd2eb53f9b3ee0c
rayaneanjos <raydosanjos98@gmail.com>
2023-03-09 18:15:24 -0300

    Hooks effect equivalente ao unmount

--- meu-app/src/App.js
+++ meu-app/src/App.js
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Header';
 import Counter from './Counter'
 function App() {
+  const [show, setShow] = useState(true)
+
+  let time = 0
+  useEffect(()=>{
+    let timer = setInterval(()=>{
+      time++
+      console.log(time)
+      if(time > 5){
+        clearInterval(timer)
+        setShow(false)
+      }
+    },1000)  
+  },[])
+
+  
+if(show){
   return (
-    <div>
-    <Header name="ProgramadorBr" links={["Sobre","Comprar","Contato"]}></Header>
-    <Counter></Counter>
+    <div> 
+    <Counter count={3}></Counter>
     </div>
   )
+}else{
   
+  return ( 
+    <div>
+    Sem Contador
+    </div>
+  )
+}
 }
 
 
