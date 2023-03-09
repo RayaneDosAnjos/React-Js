9dc1a0cd8dd01fdee9d0927eec22d619d31fc40b
rayaneanjos <raydosanjos98@gmail.com>
2023-03-09 17:55:18 -0300

    Hooks effect

--- meu-app/src/Counter.js
+++ meu-app/src/Counter.js
@@ -1,12 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Counter(props){
 
     const [count, setCount] = useState(0)
 
+    useEffect(()=> {
+        setCount(parseInt(localStorage.getItem("count")))
+        return ()=>{
+            console.log("Não tem mais contador")
+        }
+    },[])
+    useEffect(()=>  {
+        document.title = count
+        localStorage.setItem("count", count)
+    },[count])
+
+
+
     function add(){
         setCount(count+1)
-        console.log("atualizar")
+
     }
     return (
         <div>
