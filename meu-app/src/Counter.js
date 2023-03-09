9f116e4da56a5e8297eabc22291c34e56ba81a81
rayaneanjos <raydosanjos98@gmail.com>
2023-03-09 17:32:50 -0300

    Hooks estados

--- meu-app/src/Counter.js
+++ meu-app/src/Counter.js
@@ -1,27 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
 
-class Counter extends React.Component{
-    constructor(props){
-        super(props)
-        this.state = {Count: 1}
-        this.add = this.add.bind(this)
-    }
-    
+function Counter(props){
 
-    add(){ 
-      this.setState({Count: this.state.Count + 1}) 
-      
+    const [count, setCount] = useState(0)
+
+    function add(){
+        setCount(count+1)
+        console.log("atualizar")
     }
-    render(){
-        return(
+    return (
         <div>
-            <h1>Counter: {this.state.Count}</h1>
-            <button onClick={this.add}>Add</button>
+            <h1>Counter:{count}</h1>
+            <button onClick={add}>add</button>
         </div>
-        )
+    )
+}
+
+
+
+
+
+// class Counter extends React.Component{
+//     constructor(props){
+//         super(props)
+//         this.state = {Count: 1}
+//         this.add = this.add.bind(this)
+//     }
+    
+//     add(){ 
+//       this.setState({Count: this.state.Count + 1}) 
+      
+//     }
+
+//     render(){
+//         return(
+//         <div>
+//             <h1>Counter: {this.state.Count}</h1>
+//             <button onClick={this.add}>Add</button>
+//         </div>
+//         )
 
         
-    }
-}
+//     }
+// }
 
 export default Counter
