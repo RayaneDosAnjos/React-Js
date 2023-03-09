ec85f8bb5dd52c6f68792dad1572a8a6ae585ce0
rayaneanjos <raydosanjos98@gmail.com>
2023-03-08 21:58:00 -0300

    Listas

--- meu-app/src/Header.js
+++ meu-app/src/Header.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import './App.css'
+import Menu from './Menu'
 
 function Header(props){
     return (
         <header className="header">
-    <h1>{props.name}</h1>
-    <ul className="menu">
-      <li>{props.links[0]}</li>
-      <li>{props.links[1]}</li>
-      <li>{props.links[2]}</li>
-    </ul>
+            <h1>{props.name}</h1>
+            <Menu links={props.links}></Menu>
+    
   </header>
     )
 }
